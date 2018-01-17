44fbdca172f92b41d5ad8b9a60c883ccdf632eac
Kevin Shen <kevin_shen@hotmail.ca>
2018-01-17 01:26:47 -0500

    test of register form

--- js/form/googleformscript.js
+++ js/form/googleformscript.js
@@ -1,7 +1,7 @@
 var $form = $('form#test-form'),
-    url = 'https://script.google.com/macros/s/AKfycbxtusyS26tTnCcgth8FRbHNJi46tLFkP01GbPmwXbUr9DIQsxI/exec'
+  url = 'https://script.google.com/macros/s/AKfycbzjEokrAmRu8r1Y4w1VJNimznxaOZBRT3vbQzTJZOUSoLGaHcE/exec'
 
-$('#submit-form').on('click', function(e) {
+$('#submit-form').on('click', function (e) {
   e.preventDefault();
   var jqxhr = $.ajax({
     url: url,
@@ -12,3 +12,19 @@ $('#submit-form').on('click', function(e) {
     // do something
   );
 })
+
+$.fn.serializeObject = function () {
+  var o = {};
+  var a = this.serializeArray();
+  $.each(a, function () {
+    if (o[this.name]) {
+      if (!o[this.name].push) {
+        o[this.name] = [o[this.name]];
+      }
+      o[this.name].push(this.value || '');
+    } else {
+      o[this.name] = this.value || '';
+    }
+  });
+  return o;
+};
