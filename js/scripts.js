4032b254b70dc31c5e1296575a1b78f44b6aef69
Kevin Shen <kevin_shen@hotmail.ca>
2018-03-03 21:51:45 -0500

    Added updated content and register form & added validator

--- js/scripts.js
+++ js/scripts.js
@@ -27,4 +27,30 @@ $(document).ready(function () {
   navCollapseToggle.on("click", "a", null, function () {
     navCollapseToggle.collapse('hide');
   });
+  
+  // Form Validation
+  (function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+  })();
+  
+  // Particle js
+  particlesJS.load('particles-js', 'js/particles.json',
+    function() {
+      console.log('particles loaded')
+    });
 });
+
